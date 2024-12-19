b5ccf1b2596a8a94db99671e276297be02df2f83
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-12-18 21:53:41 -0800

    added a handlevalidationError

--- src/app/middlewares/globalErrorhandelrs.ts
+++ src/app/middlewares/globalErrorhandelrs.ts
@@ -3,46 +3,39 @@
 // import  httpStatus  from 'http-status';
 
 import { ErrorRequestHandler } from "express";
-import { ZodError, ZodIssue } from "zod";
+import { ZodError } from "zod";
 import config from "../config";
-import { TErrorSource } from "../interface/error";
+import handleValidationError from "../Error/handleValidationError";
+import handleZodError from "../Error/handleZodError";
+import { TErrorSources } from "../interface/error";
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let statusCode = err.statusCode || 500;
   let message = err.message || "Something went wrong!";
 
-  let errorSources: TErrorSource = [
+  let errorSources: TErrorSources = [
     {
       path: "",
       message: "Something went wrong!",
     },
   ];
 
-  const handleZodError = (err: ZodError) => {
-    const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
-      return {
-        path: issue?.path[issue.path.length - 1],
-        message: issue.message,
-      };
-    });
-    const statusCode = 400;
-    return {
-      statusCode,
-      message: " Validation Error",
-      errorSources,
-    };
-  };
-
   if (err instanceof ZodError) {
     const simplifiedError = handleZodError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === "ValidationError") {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
   }
   res.status(statusCode).json({
     success: false,
     message: message,
     errorSources,
+    // err,
     stack: config.NODE_ENV === "development" ? err?.stack : null,
   });
 };
--- src/app/modules/academicDepartment/academicDepartment.route.ts
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -5,9 +5,9 @@ import { AcademicDepartmentValidation } from "./academicDepartment.validation";
 const router = express.Router();
 router.post(
   "/create-academic-department",
-  validateRequest(
-    AcademicDepartmentValidation.createAcademicDepartmentValidationSchema
-  ),
+  // validateRequest(
+  //   AcademicDepartmentValidation.createAcademicDepartmentValidationSchema
+  // ),
   AcademicDepartmentControllers.createAcademicDepartment
 );
 router.get(
