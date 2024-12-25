62496661527fed0c012d38b1b1fcad98a6dcf3de
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-12-24 18:25:16 -0800

    added a semesterRegistrationValidation

---
+++ src/app/modules/semesterRegistation/semesterRegistation.model.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+import { TsemesterRegistration } from "./semesterRegistation.route";
+import { SemesterRegistrationStatus } from "./SemesterRegistration.constant";
+
+const semesterRegistrationSchema = new mongoose.Schema<TsemesterRegistration>(
+  {
+    academicSemester: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      unique: true,
+      ref: "AcademicSemester",
+    },
+    status: {
+      type: String,
+      enum: SemesterRegistrationStatus,
+      default: "UPCOMING",
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    minCredit: {
+      type: Number,
+      default: 3,
+    },
+    maxCredit: {
+      type: Number,
+      default: 15,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+export const SemesterRegistration = mongoose.model<TsemesterRegistration>(
+  "SemesterRegistration",
+  semesterRegistrationSchema
+);
