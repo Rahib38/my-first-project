4013714da63b995260797667e305146049920434
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-12-30 22:23:46 -0800

    create offered courses service

--- src/app/modules/offeredCourse/offeredCourse.service.ts
+++ src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -1,26 +1,102 @@
+import httpStatus from "http-status";
+import AppError from "../../Error/AppError";
+import { AcademicDepartmentModel } from "../academicDepartment/academicDepartment.model";
+import { AcademicFacultys } from "../academicFaculty/academicFaculty.model";
+import { AcademicSemester } from "../academicSemester/academicSemester.model";
+import { Course } from "../course/course.model";
+import { Faculty } from "../faculty/faculty.model";
+import { SemesterRegistration } from "../semesterRegistation/semesterRegistation.model";
 import { TOfferedCourse } from "./offeredCourse.interface";
 import { OfferedCourse } from "./offeredCourse.model";
 
-const createOfferedCourseIntoDB=async(payload:TOfferedCourse)=>{
-const result = await OfferedCourse.create(payload)
-return result
-}
+const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
+  const {
+    semesterRegistration,
+    academicSemester,
+    academicFaculty,
+    academicDepartment,
+    course,
+    section,
+    faculty,
+  } = payload;
 
-const getAllOfferedCoursesFromDB=async(query:Record<string,unknown>)=>{
+  const isSemesterRegistrationExists =
+    await SemesterRegistration.findById(semesterRegistration);
 
-}
+  if (!isSemesterRegistrationExists) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      "Semester registration not found!"
+    );
+  }
+  const academicSemesters = isSemesterRegistrationExists.academicSemester;
 
-const getSingleOfferedCourseFromDB=async(id:string)=>{
+  const isAcademicFacultyExists =
+    await AcademicFacultys.findById(academicFaculty);
 
-}
+  if (!isAcademicFacultyExists) {
+    throw new AppError(httpStatus.NOT_FOUND, "Academic Faculty not found!");
+  }
+  const isAcademicSemesterExists =
+    await AcademicSemester.findById(academicSemester);
 
-const updateOfferedCourseIntoDB=async(id:string,payload:Partial<TOfferedCourse>)=>{
+  if (!isAcademicSemesterExists) {
+    throw new AppError(httpStatus.NOT_FOUND, "Academic Semester not found!");
+  }
+  const isAcademicDepartmentExists =
+    await AcademicDepartmentModel.findById(academicDepartment);
 
-}
+  if (!isAcademicDepartmentExists) {
+    throw new AppError(httpStatus.NOT_FOUND, "Academic Department not found!");
+  }
+  const isCourseExists = await Course.findById(course);
 
-export const OfferedCoursesServices={
-    createOfferedCourseIntoDB,
-    getAllOfferedCoursesFromDB,
-    getSingleOfferedCourseFromDB,
-    updateOfferedCourseIntoDB
-}
+  if (!isCourseExists) {
+    throw new AppError(httpStatus.NOT_FOUND, "course not found!");
+  }
+  const isFacultyExists = await Faculty.findById(faculty);
+
+  if (!isFacultyExists) {
+    throw new AppError(httpStatus.NOT_FOUND, "faculty not found!");
+  }
+  const isDepartmentBelongToFaculty = await AcademicDepartmentModel.findOne({
+    academicFaculty,
+    _id: academicDepartment,
+  });
+  if (isDepartmentBelongToFaculty) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      `This ${isAcademicDepartmentExists.name} is not belong to this ${isAcademicFacultyExists.name}`
+    );
+  }
+  const isSameOfferedCourseExistsWithSameRegisteredSemesterWithSameSection =
+    await OfferedCourse.findOne({
+      semesterRegistration,
+      course,
+      section,
+    });
+  if (isSameOfferedCourseExistsWithSameRegisteredSemesterWithSameSection) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      "offered course with same section is already exist!"
+    );
+  }
+  const result = await OfferedCourse.create({ ...payload, academicSemesters });
+  return result;
+};
+
+const getAllOfferedCoursesFromDB = async (query: Record<string, unknown>) => {};
+
+const getSingleOfferedCourseFromDB = async (id: string) => {};
+
+const updateOfferedCourseIntoDB = async (
+  id: string,
+  payload: Partial<TOfferedCourse>
+) => {};
+
+export const OfferedCoursesServices = {
+  createOfferedCourseIntoDB,
+  getAllOfferedCoursesFromDB,
+  getSingleOfferedCourseFromDB,
+  updateOfferedCourseIntoDB,
+};
